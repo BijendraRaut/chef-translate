177ae5123f93f4f820ef80979f582a40ac813087
Bijendra Raut <bijendraghvm@gmail.com>
2023-04-26 10:38:41 +0530

    input,output & btn done

---
+++ app.js
@@ -0,0 +1,7 @@
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtOutput = document.querySelector("#txt-output");
+
+btnTranslate.addEventListener("click", function () {
+  console.log("clicked");
+});
