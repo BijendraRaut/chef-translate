446d7001cea1a87b04732eb7e7244453d33e9b51
Bijendra Raut <bijendraghvm@gmail.com>
2023-04-26 11:13:59 +0530

    input & output done

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ var txtInput = document.querySelector("#txt-input");
 var btnTranslate = document.querySelector("#btn-translate");
 var txtOutput = document.querySelector("#txt-output");
 
+const handleError = () => {
+  console.log("error occured", error);
+  alert("something wrong happend! try after some time");
+};
+
 btnTranslate.addEventListener("click", function () {
-  console.log("clicked");
+  var userInput = txtInput.value;
+  if (userInput === "") {
+    alert("Please enter the input first");
+    return;
+  }
+  var url = "https://api.funtranslations.com/translate/chef.json";
+  fetch(`url +"+text=" ${userInput}`)
+    .then((res) => res.json())
+    .then((json) => (txtOutput.innerText = json.contents.translated))
+    .catch(handleError);
 });
